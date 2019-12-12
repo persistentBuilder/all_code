74e4dbd307e1e378dc47e29f1911cfacac87964c
persistentBuilder <aryaman.gupta.37@gmail.com>
2019-12-12 14:16:41 +0530

    iter and download google fer

--- google_fer_siamese/dataset.py
+++ google_fer_siamese/dataset.py
@@ -1,7 +1,7 @@
 import numpy as np
 from PIL import Image
 from torch.utils.data import Dataset
-
+import requests
 
 class SiameseGoogleFer(Dataset):
     """
@@ -12,24 +12,26 @@ class SiameseGoogleFer(Dataset):
     def __init__(self, path, train_flag=True):
 
         f = open(path, "r")
-        self.all_lines = f.readlines()
+        self.all_lines = f.readlines()[0:100]
         self.all_triplets = []
         self.triplets = []
         self.train_flag = train_flag
-        if train_flag:
-            self.lines = self.all_lines[0:int(len(self.all_lines)*0.9)]
-        else:
-            self.lines = self.all_lines[int(len(self.all_lines)*0.9):len(self.all_lines)]
 
-        for line in self.lines:
+        # if train_flag:
+        #     self.lines = self.all_lines[0:int(len(self.all_lines)*0.9)]
+        # else:
+        #     self.lines = self.all_lines[int(len(self.all_lines)*0.9):len(self.all_lines)]
+        line_num = 0
+        for line in self.all_lines:
+            line_num = line_num + 1
             line_components = line.split(",")
             url_1 = line_components[0][1:-1]
             url_2 = line_components[5][1:-1]
             url_3 = line_components[10][1:-1]
             #image_name_1 = url_1.split("/")[-1]
-            img_1 = self.download_and_load_image(url_1)
-            img_2 = self.download_and_load_image(url_2)
-            img_3 = self.download_and_load_image(url_3)
+            img_1 = self.download_and_load_image(url_1, line_num)
+            img_2 = self.download_and_load_image(url_2, line_num)
+            img_3 = self.download_and_load_image(url_3, line_num)
             bounding_box_1 = []
             for j in range(1,5):
                 bounding_box_1.append(line_components[j])
@@ -42,9 +44,9 @@ class SiameseGoogleFer(Dataset):
             for j in range(11, 15):
                 bounding_box_3.append(line_components[j])
 
-            face_image_1 = self.select_face_region(img_1, bounding_box_1)
-            face_image_2 = self.select_face_region(img_2, bounding_box_2)
-            face_image_3 = self.select_face_region(img_3, bounding_box_3)
+            face_image_1 = self.select_face_region(img_1, bounding_box_1, img_1.shape[0], img_1.shape[1])
+            face_image_2 = self.select_face_region(img_2, bounding_box_2, img_2.shape[0], img_2.shape[1])
+            face_image_3 = self.select_face_region(img_3, bounding_box_3, img_3.shape[0], img_3.shape[1])
             if line_components[15] == 'TWO_CLASS_TRIPLET':
                 if line_components[17] == 1:
                     self.triplets.append([face_image_2, face_image_3, face_image_1])
@@ -53,26 +55,53 @@ class SiameseGoogleFer(Dataset):
                 else:
                     self.triplets.append([face_image_1, face_image_2, face_image_3])
 
-    def select_face_region(self, img, bounding_box):
-        pass
+    def select_face_region(self, img, bounding_box, height, width):
 
-    def download_and_save_image_from_url(self, url):
-        pass
+        top_left_column = max(0, int(bounding_box[0]*width))
+        bottom_right_column = min(width, bounding_box[1]*width)
+        top_left_row = max(0, int(bounding_box[2]*height))
+        bottom_right_row = min(height, int(bounding_box[3]*height))
+        return img[top_left_row:bottom_right_row, top_left_column:bottom_right_column, :]
 
-    def path_of_saved_image(self, url):
-        pass
+    def download_and_save_image_from_url(self, url, line_num):
 
-    def download_and_load_image(self, url):
+        image_name = url.split("/")[-1]
+        line_str = str(line_num).zfill(8) + "_"
+        image_path = "data/images/" + line_str + image_name
+
+        with open(image_path, 'wb') as handle:
+            response = requests.get(url, stream=True)
+
+            if not response.ok:
+                print(response)
+
+            for block in response.iter_content(1024):
+                if not block:
+                    break
+                handle.write(block)
+
+        return self.saved_image_load(url, line_num)
+
+
+    def saved_image_load(self, url, line_num):
+        image_name = url.split("/")[-1]
+        line_str = str(line_num).zfill(8) + "_"
+        image_path = "data/images/" + line_str + image_name
+        try:
+            np.asarray(Image.open(image_path))
+        except:
+            return None
+
+
+    def download_and_load_image(self, url, line_num):
         #check if not already downloaded
-        saved_image_path = self.path_of_saved_image(url)
-        if saved_image_path is None:
-            saved_image_path = self.download_and_save_image_from_url(url)
-        return np.asarray(Image.open(saved_image_path))
+        saved_image = self.saved_image_load(url,line_num)
+        if saved_image is None:
+            saved_image = self.download_and_save_image_from_url(url, line_num)
+        return saved_image
 
     def __getitem__(self, index):
         return self.triplets[index]
 
     def __len__(self):
         return len(self.triplets)
-
-
