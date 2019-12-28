13c581564e222b3c11ed98a81591267d57fdc923
persistentBuilder <aryaman.gupta.37@gmail.com>
2019-12-28 15:55:32 +0530

    changes for right node

--- simple_net_fer/execute_using_home.sh
+++ simple_net_fer/execute_using_home.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH -A aryaman.g
 #SBATCH --gres=gpu:2
+#SBATCH --nodelist=gnode18
 #SBATCH --mem=51000
 #SBATCH --time=4-00:00:00
 #SBATCH --mail-type=END
@@ -9,4 +10,4 @@ source ~/packages/keras_tf_venv3/bin/activate
 module load cuda/10.0
 #module load cudnn/7-cuda-10.0
 
-python main.py --epochs=100 --lr=0.0001 --net="extendNet" --dataset="affectnet"
+python main.py --epochs=100 --lr=0.0001 --batch_size=64 --net="extendNet" --dataset="affectnet"
