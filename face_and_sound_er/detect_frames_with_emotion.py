acd23ba14397b72986267b9af21722dd593513db
persisitentBuilder <aryaman.gupta.37@gmail.com>
2019-12-30 19:34:09 +0530

    changes for checking strong prediction

--- face_and_sound_er/detect_frames_with_emotion.py
+++ face_and_sound_er/detect_frames_with_emotion.py
@@ -17,28 +17,31 @@ def get_faces_from_frame(img):
     detected_faces = face_detector(img, 1)
     face_images = []
     for i, d in enumerate(detected_faces):
-        face_images.append(transform(resize_face_image(img[d.top():d.bottom(), d.left(): d.right()])))
+        face_images.append(img[d.top():d.bottom(), d.left(): d.right()])
     return face_images
 
 
 def load_model(model_path, model=None):
     checkpoint = torch.load(model_path)
-    print(checkpoint)
+    #print(checkpoint)
     model.load_state_dict(checkpoint['state_dict'])
     return model
 
 
-def strong_pred(output):
-    pass
+def strong_pred(value, prediction):
+    if value>3 and prediction!=0:
+        return True
+    return False
 
 
 def check_emotion_in_face(face_image, model=None, model_path=None):
     if model is None:
         model = load_model(model_path)
-    model.eval()
-    output = model(face_image)
-    if strong_pred(output):
-        return torch.max(output.data, 1)[0]
+        model.eval()
+    output = model(face_image.unsqueeze(0))
+    values, predictions = torch.max(output.data, 1)
+    if strong_pred(values.item(), predictions.item()):
+        return predictions.item()
     else:
         return -1
 
@@ -48,7 +51,9 @@ def check_for_lip_movement():
 
 
 def write_frame(face_img, emotion_label, video_name, frame_num):
-    cv2.imwrite(face_img, video_name+"""_{06d}_""".format(frame_num) + str(emotion_label) + ".jpg")
+    img_name_to_write = video_name+"""_{:06d}_""".format(frame_num) + str(emotion_label) + ".jpg"
+    print(img_name_to_write)
+    cv2.imwrite(img_name_to_write, face_img)
 
 
 if __name__ == '__main__':
@@ -57,7 +62,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     frame_window_for_movement = 24
     video_file = args.video_path
-    video_name = video_file.split("/", 1)[-1]
+    video_name = video_file.split("/")[-1]
     video_base_path = video_file.rsplit("/", 1)[0]
     image_resize_width = 224
     image_resize_height = 224
@@ -73,6 +78,7 @@ if __name__ == '__main__':
     model = extendNet(num_classes=8)
     model = nn.DataParallel(model)
     model = load_model(model_path="/home/aryaman.g/projects/all_code/simple_net_fer/runs/affectnet_model/model_best.pth.tar", model=model)
+    model.eval()
 
     while(cap.isOpened()):
         ret, frame = cap.read()
@@ -80,8 +86,10 @@ if __name__ == '__main__':
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = get_faces_from_frame(frame)
         for face_img in faces:
-            emotion_label = check_emotion_in_face(face_img, model=model)
-            if emotion_label >= 0:
+            print(type(face_img))
+            emotion_label = check_emotion_in_face(transform(resize_face_image(face_img)), model=model)
+            if emotion_label > 0:
+                print(frame_count)
                 write_frame(face_img, emotion_label, video_name, frame_count)
 
     cap.release()
