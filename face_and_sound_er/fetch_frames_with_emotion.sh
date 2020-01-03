172607dea1874675daacc643b6a754bdc338698b
persistentBuilder <aryaman.gupta.37@gmail.com>
2020-01-03 10:12:13 +0530

    changes for generalization

--- face_and_sound_er/fetch_frames_with_emotion.sh
+++ face_and_sound_er/fetch_frames_with_emotion.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=8-00:00:00
 #SBATCH --mail-type=END
 
-source home/aryaman.g/packages/keras_tf_venv3/bin/activate
+source /home/aryaman.g/packages/keras_tf_venv3/bin/activate
 #mkdir -p /ssd_scratch/cvit/aryaman.g/
 #mkdir -p /ssd_scratch/cvit/aryaman.g/affectnet
 python detect_emotion_while_speaking.py --video-path="/home/aryaman.g/projects/all_code/face_and_sound_er/videos/ErinBrockavich_shot_2.mp4"\
